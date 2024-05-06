5df8c354a20b0c0aa0cdb5c17d5239ff025a0c32
Kevin-12 <1719110155@utectulancingo.edu.mx>
2024-05-05 19:36:28 -0600

    extends

--- src/com/alura/java/screenmatch/Serie.java
+++ src/com/alura/java/screenmatch/Serie.java
@@ -1,5 +1,31 @@
 package src.com.alura.java.screenmatch;
 
 public class Serie {
-    
+    int temporades;
+    int episodeForSeason;
+    int minutesForEpisodi;
+
+    public int getEpisodeForSeason() {
+        return episodeForSeason;
+    }
+
+    public int getMinutesForEpisodi() {
+        return minutesForEpisodi;
+    }
+
+    public int getTemporades() {
+        return temporades;
+    }
+
+    public void setEpisodeForSeason(int episodeForSeason) {
+        this.episodeForSeason = episodeForSeason;
+    }
+
+    public void setMinutesForEpisodi(int minutesForEpisodi) {
+        this.minutesForEpisodi = minutesForEpisodi;
+    }
+
+    public void setTemporades(int temporades) {
+        this.temporades = temporades;
+    }
 }
